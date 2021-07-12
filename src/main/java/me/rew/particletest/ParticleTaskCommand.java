ecd59814934f3b680d9b1bcb0aacbe206b3718c2
rewardedcarton <rewardeddisc@gmail.com>
2021-07-12 17:28:12 +0100

    fix

--- src/main/java/me/rew/particletest/ParticleTaskCommand.java
+++ src/main/java/me/rew/particletest/ParticleTaskCommand.java
@@ -1,11 +1,16 @@
 package me.rew.particletest;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Color;
+import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ParticleTaskCommand implements CommandExecutor {
     private final ParticleTest plugin;
@@ -19,9 +24,32 @@ public class ParticleTaskCommand implements CommandExecutor {
         if (!(sender instanceof Player))
             return true;
         Player player = (Player) sender;
+        double red = Math.max(Float.MIN_VALUE, Color.BLUE.getRed() / 255);
+        double green = Color.BLUE.getGreen() / 255;
+        double blue = Color.BLUE.getBlue() / 255;
+        Location particleMiddle = player.getLocation().clone().add(0.5, 1.25, 0.5);
+        List<Location> particleCircle = getCircle(particleMiddle, 4, 50);
         Bukkit.getScheduler().runTaskTimer(plugin, () -> {
-            player.getWorld().spawnParticle(Particle.DRIP_LAVA, player.getLocation().clone(), 10, 0.25, 0.25, 0.25, 0);
-        }, 1L, 1L);
+            particleMiddle.getWorld().spawnParticle(Particle.HEART, particleMiddle, 1, 0, 0, 0, 0);
+            particleCircle.forEach(loc -> {
+                loc.getWorld().spawnParticle(Particle.REDSTONE, loc, 0, red, green, blue, 1);
+            });
+        }, 0L, 10L);
         return true;
+    }
+
+    public static List<Location> getCircle(Location center, double radius, int amount) {
+        double increment = (2 * Math.PI) / amount;
+        List<Location> locations = new ArrayList<>();
+
+        for (int i = 0; i < amount; i++) {
+            double angle = i * increment;
+            double x = center.getX() + (radius * Math.cos(angle));
+            double z = center.getZ() + (radius * Math.sin(angle));
+
+            locations.add(new Location(center.getWorld(), x, center.getY(), z));
+        }
+
+        return locations;
     }
 }
